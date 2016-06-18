07f5efea110bae56726b3732a5c9124c524f5f7f
xiangzhong.wxz <xiangzhong.wxz@alibaba-inc.com>
2016-06-18 13:40:05 +0800

    modify

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        './index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loader: 'babel',
+            include: __dirname,
+            exclude: /node_modules/
+        }, {
+            test: /\.css?$/,
+            loaders: ['style', 'raw'],
+            include: __dirname
+        }]
+    },
+    babel: {
+        cacheDirectory: false,
+        presets: [
+            "es2015", "react", "stage-2"
+        ],
+        plugins: [
+            'transform-class-properties',
+            'transform-es2015-arrow-functions',
+            'transform-es2015-block-scoping',
+            'transform-es2015-object-super'
+        ]
+    }
+}
